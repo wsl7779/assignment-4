b0db50308baa6707f8348547e2cac3a3753edf32
Will <wsl7779@gmail.com>
2023-11-18 23:56:45 -0500

    ASSINGMENT

---
+++ main.cpp
@@ -0,0 +1,426 @@
+﻿/**
+* Author: Will Lee
+* Assignment: Rise of the AI
+* Date due: 2023-11-18, 11:59pm
+* I pledge that I have completed this assignment without
+* collaborating with anyone else, in conformance with the
+* NYU School of Engineering Policies and Procedures on
+* Academic Misconduct.
+**/
+
+#define LOG(argument) std::cout << argument << '\n'
+#define STB_IMAGE_IMPLEMENTATION
+#define GL_SILENCE_DEPRECATION
+#define GL_GLEXT_PROTOTYPES 1
+#define NUMBER_OF_ENEMIES 3
+#define FIXED_TIMESTEP 0.0166666f
+#define ACC_OF_GRAVITY -9.8f
+#define LEVEL1_WIDTH 20
+#define LEVEL1_HEIGHT 5
+
+#ifdef _WINDOWS
+#include <GL/glew.h>
+#endif
+
+#include <SDL.h>
+#include <SDL_opengl.h>
+#include "glm/mat4x4.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+#include "ShaderProgram.h"
+#include "stb_image.h"
+#include "Entity.h"
+#include "Map.h"
+#include <vector>
+#include <ctime>
+#include "cmath"
+
+// ————— STRUCTS AND ENUMS —————//
+struct GameState
+{
+    Entity* player;
+    Entity* enemies;
+    Entity* projectile;
+
+    Map* map;
+};
+
+// ————— CONSTANTS ————— //
+const int WINDOW_WIDTH = 1080,
+WINDOW_HEIGHT = 720;
+
+const float BG_RED = 0.1922f,
+BG_BLUE = 0.549f,
+BG_GREEN = 0.9059f,
+BG_OPACITY = 1.0f;
+
+const int VIEWPORT_X = 0,
+VIEWPORT_Y = 0,
+VIEWPORT_WIDTH = WINDOW_WIDTH,
+VIEWPORT_HEIGHT = WINDOW_HEIGHT;
+
+const char V_SHADER_PATH[] = "shaders/vertex_textured.glsl",
+F_SHADER_PATH[] = "shaders/fragment_textured.glsl";
+
+const float MILLISECONDS_IN_SECOND = 1000.0;
+const char SPRITESHEET_FILEPATH[] = "assets/yuuka trans.png";
+const char ENEMY_FILEPATH[] = "assets/youdied.jpg";
+const char MAP_TILESET_FILEPATH[] = "assets/tileset.png";
+const char TEXT_FILEPATH[] = "assets/font1.png";
+
+GLuint g_text_texture_id;
+
+const int FONTBANK_SIZE = 16;
+const int NUMBER_OF_TEXTURES = 1;
+const GLint LEVEL_OF_DETAIL = 0;
+const GLint TEXTURE_BORDER = 0;
+
+// ————— VARIABLES ————— //
+GameState g_game_state;
+
+SDL_Window* g_display_window;
+bool g_game_is_running = true;
+bool g_win = false;
+bool g_lose = false;
+
+ShaderProgram g_shader_program;
+glm::mat4 g_view_matrix, g_projection_matrix;
+
+float g_previous_ticks = 0.0f;
+float g_time_accumulator = 0.0f;
+
+unsigned int LEVEL_1_DATA[] =
+{
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 54, 54, 54, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    1, 3, 0, 0, 0, 0, 0, 0, 0, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3,
+    23, 89, 2, 3, 0, 0, 1, 2, 2, 90, 24, 24, 24, 24, 24, 24, 24, 24, 24, 25,
+    23, 24, 24, 25, 0, 0, 23, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 25
+};
+
+
+// ———— GENERAL FUNCTIONS ———— //
+GLuint load_texture(const char* filepath)
+{
+    int width, height, number_of_components;
+    unsigned char* image = stbi_load(filepath, &width, &height, &number_of_components, STBI_rgb_alpha);
+
+    if (image == NULL)
+    {
+        LOG("Unable to load image. Make sure the path is correct.");
+        assert(false);
+    }
+
+    GLuint textureID;
+    glGenTextures(NUMBER_OF_TEXTURES, &textureID);
+    glBindTexture(GL_TEXTURE_2D, textureID);
+    glTexImage2D(GL_TEXTURE_2D, LEVEL_OF_DETAIL, GL_RGBA, width, height, TEXTURE_BORDER, GL_RGBA, GL_UNSIGNED_BYTE, image);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+    stbi_image_free(image);
+
+    return textureID;
+}
+void draw_text(ShaderProgram* program, GLuint font_texture_id, std::string text, float screen_size, float spacing, glm::vec3 position)
+{
+    // Scale the size of the fontbank in the UV-plane
+    // We will use this for spacing and positioning
+    float width = 1.0f / FONTBANK_SIZE;
+    float height = 1.0f / FONTBANK_SIZE;
+
+    // Instead of having a single pair of arrays, we'll have a series of pairs—one for each character
+    // Don't forget to include <vector>!
+    std::vector<float> vertices;
+    std::vector<float> texture_coordinates;
+
+    // For every character...
+    for (int i = 0; i < text.size(); i++) {
+        // 1. Get their index in the spritesheet, as well as their offset (i.e. their position
+        //    relative to the whole sentence)
+        int spritesheet_index = (int)text[i];  // ascii value of character
+        float offset = (screen_size + spacing) * i;
+
+        // 2. Using the spritesheet index, we can calculate our U- and V-coordinates
+        float u_coordinate = (float)(spritesheet_index % FONTBANK_SIZE) / FONTBANK_SIZE;
+        float v_coordinate = (float)(spritesheet_index / FONTBANK_SIZE) / FONTBANK_SIZE;
+
+        // 3. Inset the current pair in both vectors
+        vertices.insert(vertices.end(), {
+            offset + (-0.5f * screen_size), 0.5f * screen_size,
+            offset + (-0.5f * screen_size), -0.5f * screen_size,
+            offset + (0.5f * screen_size), 0.5f * screen_size,
+            offset + (0.5f * screen_size), -0.5f * screen_size,
+            offset + (0.5f * screen_size), 0.5f * screen_size,
+            offset + (-0.5f * screen_size), -0.5f * screen_size,
+            });
+
+        texture_coordinates.insert(texture_coordinates.end(), {
+            u_coordinate, v_coordinate,
+            u_coordinate, v_coordinate + height,
+            u_coordinate + width, v_coordinate,
+            u_coordinate + width, v_coordinate + height,
+            u_coordinate + width, v_coordinate,
+            u_coordinate, v_coordinate + height,
+            });
+    }
+
+    // 4. And render all of them using the pairs
+    glm::mat4 model_matrix = glm::mat4(1.0f);
+    model_matrix = glm::translate(model_matrix, position);
+
+    program->set_model_matrix(model_matrix);
+    glUseProgram(program->get_program_id());
+
+    glVertexAttribPointer(program->get_position_attribute(), 2, GL_FLOAT, false, 0, vertices.data());
+    glEnableVertexAttribArray(program->get_position_attribute());
+    glVertexAttribPointer(program->get_tex_coordinate_attribute(), 2, GL_FLOAT, false, 0, texture_coordinates.data());
+    glEnableVertexAttribArray(program->get_tex_coordinate_attribute());
+
+    glBindTexture(GL_TEXTURE_2D, font_texture_id);
+    glDrawArrays(GL_TRIANGLES, 0, (int)(text.size() * 6));
+
+    glDisableVertexAttribArray(program->get_position_attribute());
+    glDisableVertexAttribArray(program->get_tex_coordinate_attribute());
+}
+
+void initialise()
+{
+    SDL_Init(SDL_INIT_VIDEO);
+    g_display_window = SDL_CreateWindow("its the exploder killers",
+        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+        WINDOW_WIDTH, WINDOW_HEIGHT,
+        SDL_WINDOW_OPENGL);
+
+    SDL_GLContext context = SDL_GL_CreateContext(g_display_window);
+    SDL_GL_MakeCurrent(g_display_window, context);
+
+#ifdef _WINDOWS
+    glewInit();
+#endif
+
+    glViewport(VIEWPORT_X, VIEWPORT_Y, VIEWPORT_WIDTH, VIEWPORT_HEIGHT);
+
+    g_shader_program.load(V_SHADER_PATH, F_SHADER_PATH);
+
+    g_view_matrix = glm::mat4(1.0f);
+    g_projection_matrix = glm::ortho(-5.0f, 5.0f, -3.75f, 3.75f, -1.0f, 1.0f);
+
+    g_shader_program.set_projection_matrix(g_projection_matrix);
+    g_shader_program.set_view_matrix(g_view_matrix);
+
+    glUseProgram(g_shader_program.get_program_id());
+
+    glClearColor(BG_RED, BG_BLUE, BG_GREEN, BG_OPACITY);
+
+    // ————— MAP SET-UP ————— //
+    GLuint map_texture_id = load_texture(MAP_TILESET_FILEPATH);
+    g_game_state.map = new Map(LEVEL1_WIDTH, LEVEL1_HEIGHT, LEVEL_1_DATA, map_texture_id, 1.0f, 22, 7);
+
+    g_text_texture_id = load_texture(TEXT_FILEPATH);
+
+    // ————— PLAYER ————— //
+
+    g_game_state.player = new Entity;
+    g_game_state.player->set_entity_type(PLAYER);
+    g_game_state.player->set_position(glm::vec3(0.0f, 2.0f, 0.0f));
+    g_game_state.player->set_movement(glm::vec3(0.0f));
+    g_game_state.player->set_acceleration(glm::vec3(0.0f, ACC_OF_GRAVITY, 0.0f));
+    g_game_state.player->set_wh(1.0f, 1.0f);
+    g_game_state.player->set_speed(2.5f);
+    g_game_state.player->m_jumping_power = 5.0f;
+    g_game_state.player->m_texture_id = load_texture(SPRITESHEET_FILEPATH);
+
+    // ————— ENEMY ————— //
+    GLuint enemy_texture_id = load_texture(ENEMY_FILEPATH);
+
+    g_game_state.enemies = new Entity[NUMBER_OF_ENEMIES];
+    g_game_state.enemies[0].set_entity_type(ENEMY);
+    g_game_state.enemies[0].set_ai_type(WALKER);
+    g_game_state.enemies[0].set_ai_state(CHILL);
+    g_game_state.enemies[0].m_texture_id = enemy_texture_id;
+    g_game_state.enemies[0].set_position(glm::vec3(5.0f, 3.0f, 0.0f));
+    g_game_state.enemies[0].set_movement(glm::vec3(0.0f));
+    g_game_state.enemies[0].set_speed(1.0f);
+    g_game_state.enemies[0].set_acceleration(glm::vec3(0.0f, ACC_OF_GRAVITY, 0.0f));
+
+
+    g_game_state.enemies[1].set_entity_type(ENEMY);
+    g_game_state.enemies[1].set_ai_type(JUMPER);
+    g_game_state.enemies[1].set_ai_state(JUMP);
+    g_game_state.enemies[1].m_jumping_power = 10.0f;
+    g_game_state.enemies[1].m_texture_id = enemy_texture_id;
+    g_game_state.enemies[1].set_position(glm::vec3(10.0f, 0.0f, 0.0f));
+    g_game_state.enemies[1].set_movement(glm::vec3(0.0f));
+    g_game_state.enemies[1].set_speed(2.0f);
+    g_game_state.enemies[1].set_acceleration(glm::vec3(0.0f, ACC_OF_GRAVITY, 0.0f));
+
+    g_game_state.enemies[2].set_entity_type(ENEMY);
+    g_game_state.enemies[2].set_ai_type(BOSS);
+    g_game_state.enemies[2].set_ai_state(BOSSFIGHT);
+    g_game_state.enemies[2].m_texture_id = enemy_texture_id;
+    g_game_state.enemies[2].set_position(glm::vec3(19.0f, 0.0f, 0.0f));
+    g_game_state.enemies[2].set_movement(glm::vec3(0.0f));
+    g_game_state.enemies[2].set_speed(1.0f);
+    g_game_state.enemies[2].set_acceleration(glm::vec3(0.0f, ACC_OF_GRAVITY, 0.0f));
+
+    // ————— GENERAL ————— //
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+}
+
+void process_input()
+{
+    // VERY IMPORTANT: If nothing is pressed, we don't want to go anywhere
+    g_game_state.player->set_movement(glm::vec3(0.0f));
+    g_game_state.player->set_acceleration(glm::vec3(0.0f, ACC_OF_GRAVITY, 0.0f));
+
+
+    SDL_Event event;
+    while (SDL_PollEvent(&event))
+    {
+        switch (event.type) {
+        case SDL_QUIT:
+        case SDL_WINDOWEVENT_CLOSE:
+            g_game_is_running = false;
+            break;
+
+        case SDL_KEYDOWN:
+            switch (event.key.keysym.sym) {
+            case SDLK_ESCAPE: g_game_is_running = false;
+                break;
+
+            case SDLK_SPACE:
+                // Jump
+                if (g_game_state.player->m_collided_bottom)
+                {
+                    g_game_state.player->m_is_jumping = true;
+                }
+                break;
+
+            default:
+                break;
+            }
+        }
+    }
+
+    const Uint8* key_state = SDL_GetKeyboardState(NULL);
+
+    if (key_state[SDL_SCANCODE_LEFT])
+    {
+        g_game_state.player->move_left();
+    }
+    else if (key_state[SDL_SCANCODE_RIGHT])
+    {
+        g_game_state.player->move_right();
+    }
+    if (key_state[SDL_SCANCODE_UP])
+    {
+
+    }
+
+    if (glm::length(g_game_state.player->get_movement()) > 1.0f)
+    {
+        g_game_state.player->set_movement(glm::normalize(g_game_state.player->get_movement()));
+    }
+}
+
+void update()
+{
+
+    if (g_game_state.player->get_killed() == NUMBER_OF_ENEMIES) {
+        g_win = true;
+    }
+    else if (g_game_state.player->get_lose()) {
+        g_lose = true;
+    }
+    else {
+        float ticks = (float)SDL_GetTicks() / MILLISECONDS_IN_SECOND;
+        float delta_time = ticks - g_previous_ticks;
+        g_previous_ticks = ticks;
+
+        // ————— FIXED TIMESTEP ————— //
+        // STEP 1: Keep track of how much time has passed since last step
+        delta_time += g_time_accumulator;
+
+        // STEP 2: Accumulate the ammount of time passed while we're under our fixed timestep
+        if (delta_time < FIXED_TIMESTEP)
+        {
+            g_time_accumulator = delta_time;
+            return;
+        }
+
+        // STEP 3: Once we exceed our fixed timestep, apply that elapsed time into the objects' update function invocation
+        while (delta_time >= FIXED_TIMESTEP)
+        {
+            g_game_state.player->update(FIXED_TIMESTEP, g_game_state.player, g_game_state.enemies, NUMBER_OF_ENEMIES, g_game_state.map);
+
+            for (int i = 0; i < NUMBER_OF_ENEMIES; i++)
+            {
+                g_game_state.enemies[i].update(FIXED_TIMESTEP, g_game_state.enemies, g_game_state.player, NUMBER_OF_ENEMIES, g_game_state.map);
+            }
+
+            delta_time -= FIXED_TIMESTEP;
+        }
+
+        g_time_accumulator = delta_time;
+
+        g_view_matrix = glm::mat4(1.0f);
+        g_view_matrix = glm::translate(g_view_matrix, glm::vec3(-g_game_state.player->get_position().x, 0.0f, 0.0f));
+    }
+}
+
+void render()
+{
+    g_shader_program.set_view_matrix(g_view_matrix);
+
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    g_game_state.player->render(&g_shader_program);
+    g_game_state.map->render(&g_shader_program);
+    for (int i = 0; i < NUMBER_OF_ENEMIES; i++)
+    {
+        g_game_state.enemies[i].render(&g_shader_program);
+    }
+
+    draw_text(&g_shader_program, g_text_texture_id, std::string("HELLO"), 0.25f, 0.0f, glm::vec3(0.0f, 0.0f, 0.0f));
+
+    if (g_win) {
+        draw_text(&g_shader_program, g_text_texture_id, std::string("YOU WIN!!!!"), 0.5f, 0.0f, glm::vec3(g_game_state.player->get_position().x - 1.5f, 2.0f, 0.0f));
+    }
+
+    if (g_lose) {
+        draw_text(&g_shader_program, g_text_texture_id, std::string("YOU LOSE HAHA!!!!"), 0.5f, 0.0f, glm::vec3(g_game_state.player->get_position().x - 4.0f, 2.0f, 0.0f));
+    }
+
+    SDL_GL_SwapWindow(g_display_window);
+}
+
+void shutdown()
+{
+    SDL_Quit();
+
+    delete[] g_game_state.enemies;
+    delete    g_game_state.player;
+    delete    g_game_state.map;
+
+}
+
+// ————— DRIVER GAME LOOP ————— /
+int main(int argc, char* argv[])
+{
+    initialise();
+
+    while (g_game_is_running)
+    {
+        process_input();
+        update();
+        render();
+    }
+
+    shutdown();
+    return 0;
+}
